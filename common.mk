ae6e388baf1f1f300524e625bcdca6edfe9bf307
Vachounet <vachounet@live.fr>
2021-01-27 15:32:48 +0100

    sm8250-common: build android.hidl.base@1.0 and android.hidl.manager@1.0
    Change-Id: I7d78eb4e3791ae35daba6726e0e0185cd900eebc

--- common.mk
+++ common.mk
@@ -113,6 +113,8 @@ PRODUCT_PACKAGES += \
 
 # HIDL
 PRODUCT_PACKAGES += \
+    android.hidl.base@1.0 \
+    android.hidl.manager@1.0 \
     libhidltransport \
     libhwbinder
 
@@ -141,9 +143,6 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     vndk_package
-
-PRODUCT_PACKAGES += \
-    android.hidl.base@1.0
 
 # Remove unwanted packages
 PRODUCT_PACKAGES += \
