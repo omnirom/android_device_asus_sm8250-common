a3b69732f54e6cf12ecb966a760d04f06bead1b1
micky387 <mickaelsaibi@free.fr>
2021-04-15 23:37:15 +0200

    sm8250-common: Add refresh rate tile
    Change-Id: I61226cc31f26c8d81ff2a5141abe034524eeb6fe

--- DeviceParts/src/org/omnirom/device/DeviceSettings.java
+++ DeviceParts/src/org/omnirom/device/DeviceSettings.java
@@ -19,11 +19,16 @@ package org.omnirom.device;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.Context;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.content.res.Resources;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.os.Parcel;
+import android.os.RemoteException;
+import android.os.ServiceManager;
 import androidx.preference.PreferenceFragment;
 import androidx.preference.ListPreference;
 import androidx.preference.Preference;
@@ -38,6 +43,7 @@ import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
 import android.util.Log;
+import java.util.Map;
 
 public class DeviceSettings extends PreferenceFragment implements
         Preference.OnPreferenceChangeListener {
@@ -47,13 +53,54 @@ public class DeviceSettings extends PreferenceFragment implements
 
     private static final String KEY_CATEGORY_SCREEN = "screen";
     public static final String KEY_GAME_GENIE = "game_toolbar_app";
+    private static final String KEY_FRAME_MODE = "frame_mode_key";
+    private static final String KEY_FRAME_MODE_ZF7 = "frame_mode_key_zf7";
+    private static final String KEY_FRAME_CATEGORY = "frame_mode_main";
+    private static final String KEY_FRAME_CATEGORY_ZF7 = "frame_mode_main_zf7";
+    public static final String FPS = "fps";
 
+    protected static final String DEFAULT_FPS_VALUE = "60";
+    private static ListPreference mFrameModeRate;
+    private static ListPreference mFrameModeRateZF7;
     private static TwoStatePreference mGloveModeSwitch;
     private static Preference mGameGenie;
+
+    private static final boolean isRog3 = android.os.Build.DEVICE.equals("ASUS_I003_1");
+    private static final String SURFACE_FLINGER_SERVICE_KEY = "SurfaceFlinger";
+    private static final String SURFACE_COMPOSER_INTERFACE_KEY = "android.ui.ISurfaceComposer";
+    private static final int SURFACE_FLINGER_CODE = 1035;
+    private static Map<Integer, Integer> fpsMap = Map.of(144, 0, 120, 1, 90, 2, 60, 3);
+    private static Map<Integer, Integer> fpsMapZF7 = Map.of(90, 0, 60, 1);
+    private static IBinder mSurfaceFlinger;
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.main, rootKey);
+        final PreferenceScreen prefScreen = getPreferenceScreen();
+        final PreferenceCategory fpsCategory =
+                (PreferenceCategory) prefScreen.findPreference(KEY_FRAME_CATEGORY);
+        final PreferenceCategory fpsCategoryzf7 =
+                (PreferenceCategory) prefScreen.findPreference(KEY_FRAME_CATEGORY_ZF7);
+
+        mFrameModeRate = (ListPreference) findPreference(KEY_FRAME_MODE);
+        int framevalue = Settings.System.getInt(getContext().getContentResolver(),
+                            FPS, 60);
+        mFrameModeRate.setValue(Integer.toString(framevalue));
+        mFrameModeRate.setSummary(mFrameModeRate.getEntry());
+        mFrameModeRate.setOnPreferenceChangeListener(this);
+
+        mFrameModeRateZF7 = (ListPreference) findPreference(KEY_FRAME_MODE_ZF7);
+        int framevaluezf7 = Settings.System.getInt(getContext().getContentResolver(),
+                            FPS, 60);
+        mFrameModeRateZF7.setValue(Integer.toString(framevaluezf7));
+        mFrameModeRateZF7.setSummary(mFrameModeRateZF7.getEntry());
+        mFrameModeRateZF7.setOnPreferenceChangeListener(this);
+
+        if (isRog3) {
+            prefScreen.removePreference(fpsCategoryzf7);
+        } else {
+            prefScreen.removePreference(fpsCategory);
+        }
 
         mGloveModeSwitch = (TwoStatePreference) findPreference(KEY_GLOVE_SWITCH);
         mGloveModeSwitch.setEnabled(GloveModeSwitch.isSupported());
@@ -72,6 +119,46 @@ public class DeviceSettings extends PreferenceFragment implements
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mFrameModeRate) {
+            int value = Integer.valueOf((String) newValue);
+            int index = mFrameModeRate.findIndexOfValue((String) newValue);
+            mFrameModeRate.setSummary(mFrameModeRate.getEntries()[index]);
+            changeFps(getContext(), value);
+            Settings.System.putInt(getContext().getContentResolver(), FPS, value);
+        }
+        if (preference == mFrameModeRateZF7) {
+            int value = Integer.valueOf((String) newValue);
+            int index = mFrameModeRateZF7.findIndexOfValue((String) newValue);
+            mFrameModeRateZF7.setSummary(mFrameModeRateZF7.getEntries()[index]);
+            changeFps(getContext(), value);
+            Settings.System.putInt(getContext().getContentResolver(), FPS, value);
+        }
         return true;
+    }
+
+    protected static void changeFps(Context context, int fps) {
+        mSurfaceFlinger = ServiceManager.getService(SURFACE_FLINGER_SERVICE_KEY);
+        try {
+            if (mSurfaceFlinger != null) {
+                mSurfaceFlinger = ServiceManager.getService(SURFACE_FLINGER_SERVICE_KEY);
+                Parcel data = Parcel.obtain();
+                data.writeInterfaceToken(SURFACE_COMPOSER_INTERFACE_KEY);
+                if (isRog3) {
+                    data.writeInt(fpsMap.getOrDefault(fps, -1));
+                } else {
+                    data.writeInt(fpsMapZF7.getOrDefault(fps, -1));
+                }
+                mSurfaceFlinger.transact(SURFACE_FLINGER_CODE, data, null, 0);
+                data.recycle();
+                if (isRog3) {
+                    System.out.println("OmnifpsMap: " + fpsMap +" Omnifps: "+ fps);
+                } else {
+                    System.out.println("OmnifpsMap: " + fpsMapZF7 +" Omnifps: "+ fps);
+                }
+            }
+        } catch (RemoteException ex) {
+               // intentional no-op
+        }
+            Settings.System.putInt(context.getContentResolver(), DeviceSettings.FPS, fps);
     }
 }
