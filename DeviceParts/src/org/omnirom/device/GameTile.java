0274f54f90c190a0e3bf52006ef658f4b38530d2
micky387 <mickaelsaibi@free.fr>
2021-07-29 02:49:43 +0200

    sm8250-common: Add GameGenie Tile
    Change-Id: I329648ec732e44367ef1faf0ae8b203e8f5d2e33

---
+++ DeviceParts/src/org/omnirom/device/GameTile.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (C) 2021 The OmniROM Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.omnirom.device;
+
+import android.content.Intent;
+import android.graphics.drawable.Icon;
+import android.service.quicksettings.Tile;
+import android.service.quicksettings.TileService;
+
+public class GameTile extends TileService {
+
+    private static final String BROADCAST_ACTION_LAUNCH_PANEL = "com.asus.gamewidget.action.SHOW_WIDGET_BTN";
+    private static final String EXTRA_LAUNCH_PANEL_DISPLAY = "clicked_source";
+    private static final String EXTRA_LAUNCH_PANEL_SIDE = "side";
+
+    @Override
+    public void onStartListening() {
+        super.onStartListening();
+
+        getQsTile().setIcon(Icon.createWithResource(this, R.drawable.game_genie));
+        getQsTile().setState(Tile.STATE_ACTIVE);
+        getQsTile().updateTile();
+    }
+
+    @Override
+    public void onClick() {
+        super.onClick();
+
+        Intent intent = new Intent(BROADCAST_ACTION_LAUNCH_PANEL);
+        intent.setPackage("com.asus.gamewidget");
+        intent.putExtra("clicked_source", 1);
+        intent.putExtra("side", false);
+        intent.addFlags(268435456);
+        this.sendBroadcast(intent);
+    }
+}
