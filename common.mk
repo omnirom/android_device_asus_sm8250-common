88d2edb5d59a6f7e3dc7b2b8dfb213efada94d7c
micky387 <mickaelsaibi@free.fr>
2022-09-30 22:33:48 +0200

    sm8250-common: Switch to A13 Qcom display repo
    Change-Id: I1fd0e49084ef85da8a07f5eb221e4a6b2dd40303

--- common.mk
+++ common.mk
@@ -219,7 +219,8 @@ PRODUCT_PACKAGES += \
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH)
+    $(LOCAL_PATH) \
+    vendor/qcom/opensource/display
 
 # Systemhelper
 PRODUCT_PACKAGES += \
