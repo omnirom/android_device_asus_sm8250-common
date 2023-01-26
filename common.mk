37674568351109b03cb62fb3d177be09a670eab0
micky387 <mickaelsaibi@free.fr>
2023-01-26 18:11:29 +0100

    sm8250-common: Import Display HAL stuff from rog3
    Change-Id: Idb6c0e10f4988bbffe22f5b43e65191b3bbbcd49

--- common.mk
+++ common.mk
@@ -109,16 +109,36 @@ PRODUCT_PACKAGES += \
 
 # Display
 PRODUCT_PACKAGES += \
+    android.hardware.graphics.mapper@3.0-impl-qti-display \
+    android.hardware.graphics.mapper@4.0-impl-qti-display \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
+    android.hardware.renderscript@1.0-impl \
+    gralloc.kona \
     libion \
     libtinyalsa \
-    libtinyxml2
+    libtinyxml2 \
+    libqdutils \
+    libqservice \
+    libsdmcore \
+    libsdmutils \
+    libvulkan \
+    memtrack.kona \
+    vendor.qti.hardware.display.allocator-service \
+    vendor.qti.hardware.display.composer-service \
+    vendor.qti.hardware.display.mapper@1.0.vendor \
+    vendor.qti.hardware.display.mapper@1.1.vendor \
+    vendor.qti.hardware.display.mapper@2.0.vendor \
+    vendor.qti.hardware.display.mapper@3.0.vendor \
+    vendor.qti.hardware.display.mapper@4.0.vendor
 
 $(call inherit-product, vendor/qcom/opensource/display/config/display-product-vendor.mk)
 $(call inherit-product, vendor/qcom/opensource/commonsys/display/config/display-product-commonsys.mk)
 $(call inherit-product, vendor/qcom/opensource/commonsys-intf/display/config/display-interfaces-product.mk)
 $(call inherit-product, vendor/qcom/opensource/commonsys-intf/display/config/display-product-system.mk)
+
+-include hardware/qcom-caf/sm8250/display/config/display-board.mk
+-include hardware/qcom-caf/sm8250/display/config/display-product.mk
 
 # DRM
 PRODUCT_PACKAGES += \
@@ -216,7 +236,8 @@ PRODUCT_PACKAGES += \
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH)
+    $(LOCAL_PATH) \
+    vendor/omni/build/soong
 
 # Systemhelper
 PRODUCT_PACKAGES += \
