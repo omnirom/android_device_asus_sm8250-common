ee91407c1b78e9533be28fc219f0b0524d5def47
micky387 <mickaelsaibi@free.fr>
2023-11-02 12:14:08 +0100

    sm8250-common: build android.frameworks.sensorservice@1.0.vendor for camera
     linker  : CANNOT LINK EXECUTABLE "/vendor/bin/hw/qvrservice": library "android.frameworks.sensorservice@1.0.so" not found: needed by /vendor/lib64/libqvrservice.so in namespace (default)
    
    Change-Id: Id555f3a874dad88d0a4b552a393b437842b6c0c5

--- common.mk
+++ common.mk
@@ -92,6 +92,7 @@ PRODUCT_PACKAGES_DEBUG += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    android.frameworks.sensorservice@1.0.vendor \
     android.hardware.camera.provider@2.4 \
     android.hardware.camera.device@3.4
 
