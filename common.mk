f3468976ad9935f1ac19d5b274ca715e6859d86f
micky387 <mickaelsaibi@free.fr>
2023-11-27 09:54:30 +0100

    sm8250-common: Copy bluetooth and bluetooth_le from aosp source
    Change-Id: I3741c82e112ec7c3276b71b831050606aabffac0

--- common.mk
+++ common.mk
@@ -78,6 +78,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0.vendor
 
+PRODUCT_COPY_FILES +=\
+    frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
+    frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml
+
 # Boot control
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.1-impl.recovery \
