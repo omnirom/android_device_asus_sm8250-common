fcf5ed30f960288fef42872ce800fd5fad2be1d5
micky387 <mickaelsaibi@free.fr>
2023-11-27 15:02:42 +0100

    sm8250-common: Drop Qcom WFD
    Change-Id: I89e37132571afd23fa0985e0426e3b0718b8da30

--- common.mk
+++ common.mk
@@ -304,14 +304,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
     hostapd \
-    libavservices_minijail \
     libavservices_minijail.vendor \
     libavservices_minijail_vendor \
-    libnl \
     libwifi-hal-qcom \
     libwpa_client \
     wpa_supplicant \
     wpa_supplicant.conf
-
-PRODUCT_BOOT_JARS += \
-    WfdCommon
