2c1d129f60e2bb906230dc4109ba79835789ef22
micky387 <mickaelsaibi@free.fr>
2023-11-27 15:01:57 +0100

    sm8250-common: Build audio bluetooth 2.1 from source
    Change-Id: I3fbd1dd3446bf3d9002e189f042d7e1c74932092

--- common.mk
+++ common.mk
@@ -79,7 +79,9 @@ PRODUCT_PACKAGES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.0.vendor
+    android.hardware.bluetooth@1.0.vendor \
+    android.hardware.bluetooth.audio@2.1-impl \
+    audio.bluetooth.default
 
 PRODUCT_COPY_FILES +=\
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
