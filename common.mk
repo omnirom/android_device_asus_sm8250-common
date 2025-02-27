de5aac7a6af441b34f9f46eb0f82c927d14f124e
micky387 <mickaelsaibi@free.fr>
2025-02-27 10:13:09 -0500

    sm8250-common: Build gralloc.qcom
    It was renamed in the display hw source
    
    Change-Id: I506051fe1d9779ce962dfe2dc4dec3fff4fc6716
    Signed-off-by: micky387 <mickaelsaibi@free.fr>

--- common.mk
+++ common.mk
@@ -129,7 +129,7 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
     android.hardware.graphics.mapper@4.0-impl-qti-display \
     android.hardware.renderscript@1.0-impl \
-    gralloc.kona \
+    gralloc.qcom \
     libion \
     libtinyalsa \
     libtinyxml2 \
